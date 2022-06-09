dc7e163a3ec6fd6171a08a4d3716a47917f842f1
yilmazhasan <yilmazhasan111@gmail.com>
2022-06-09 19:39:17 +0300

    Make test fail

--- test/test2.js
+++ test/test2.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 describe('A simple test', function() {
-    it('should show 1 is not eq to 0', function() {
+    it('should show 1 is not eq to 1', function() {
       assert.notEqual(1, 1);
     })
 })
