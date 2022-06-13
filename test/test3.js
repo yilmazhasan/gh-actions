44ecc3c12766531e65d23fa9047c0da2aa3da4be
yilmazhasan <yilmazhasan111@gmail.com>
2022-06-13 20:44:49 +0300

    Add test3

---
+++ test/test3.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('SECRET_KEY', function() {
+    it('ACTION_TEST_SECRET should be correct', function() {
+      assert.equal(process.env, 'ThisIsMyActionTestSecret');
+    })
+})
