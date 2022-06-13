65e1c13fd4372ab0ec7a213bfce9e892b524579e
yilmazhasan <yilmazhasan111@gmail.com>
2022-06-13 21:05:01 +0300

    Test env

--- test/test3.js
+++ test/test3.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 describe('SECRET_KEY', function() {
     it('ACTION_TEST_SECRET should be correct', function() {
+      console.log({ processEnvACTION_TEST_SECRET: process.env.ACTION_TEST_SECRET })
       assert.equal(process.env.ACTION_TEST_SECRET, 'ThisIsMyActionTestSecret');
     })
 })
